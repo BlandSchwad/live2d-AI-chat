6adca246f4d13543cbe927261bb6681f8e077645
blandschwad <blandschwad@gmail.com>
2025-03-26 10:19:19 -0700

    auth

--- src/main.tsx
+++ src/main.tsx
@@ -8,27 +8,34 @@ import Header from './components/Header.tsx'
 import {BrowserRouter, Routes, Route} from "react-router"
 import Chat from './components/Chat.tsx'
 import Music from './components/Music.tsx'
+import LoginButton from "./components/auth/login"
+import Profile from './components/auth/profile.tsx'
+import { Auth0ProviderWithNavigate } from './components/auth/provideriwthnav.tsx'
 
 const dummyOptions = ['Home', "Chat", 'About', 'Projects', 'Music']
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    
     <BrowserRouter>
-
+    <Auth0ProviderWithNavigate>
     
     <Header options={dummyOptions} />
     <Routes>
       <Route path='/chat' element={<Chat/>}/>
-      <Route path='/home' element={<App/>}/>
+      {/* <Route path={'/home'} element={<App/>}/> */}
+
       <Route path='/music' element={<Music/>}/>
+      <Route path='/' element={<App/>} />
     </Routes>
-
-
+    
     
     {/* <App /> */}
      
-    
-    
+    </Auth0ProviderWithNavigate>
+   
     </BrowserRouter>
     
+
   </StrictMode>,
 )
